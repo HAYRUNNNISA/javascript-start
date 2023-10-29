ba09f7ae3105c78ec540bf1a9a19045e9512b51e
Hayrun Nisa ÇELİK <hayrun.celik@ogr.sakarya.edu.tr>
2023-10-29 16:03:37 +0300

    Add style props

--- 1.js
+++ 1.js
@@ -1,1 +1,42 @@
-window.alert(z);
+
+console.log("hayrunnisa")
+var ad_soyad, kullanici_adi;
+         var x = 5;
+         var y =37;
+         var z = x *y;
+         document.write(z);
+         var arabalar= ["Mercedes", "BMW","Audi"];
+         document.write(arabalar);
+         let a =733;
+         let b =82738;
+         console.log(a+b)
+         console.log(window);
+         alert("merhaba");/*popup çıkartmak için*/
+         let c=373.737;
+         console.log(typeof c)
+
+
+    document.getElementById("demo").innerHTML = 5 + 6
+    console.log(7 + 4);
+    
+    var carName = "Volvo";
+    function mOver(obj) {
+obj.innerHTML = "thanks…"
+}
+function mOut(obj) {
+obj.innerHTML = " Bring the Mouse to Me …"
+}
+    document.getElementsByClassName(name)
+document.getElementById("demo").innerHTML = carName;
+document.createElement(element);
+document.getElementById(id).onclick =
+function(){commands}
+document.getElementById("demo").innerHTML="Merhaba Arkadaşlar";
+var element = document.getElementById("header");
+
+document.getElementById("myImage").src = "desktop-wallpaper-batman-dark-knight-fire-logo-batman-for-pc";
+
+element.innerHTML = "New Title";
+var arabalar= ["Mercedes"
+, "BMW",
+"Audi"];
